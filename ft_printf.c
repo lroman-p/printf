38475a28a29580aeea90b7ffbc59f8247adef28d
lroman-p <145353783+lroman-p@users.noreply.github.com>
2024-03-21 11:50:02 +0100

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -37,27 +37,27 @@ int	ft_select_format(va_list argument, const char word)
 
 int	ft_printf(const char *str, ...)
 {
-	int		i;		/*se declara una variable entera i que se usara como indice para recorrer la cadena str*/
-	int		size;		/*se declara una variable size que se usara para almacenar el tamaño de los caracteres impresos*/
-	va_list	argument;		/* se declara una variable tipo va_list llamada argument, que sirve para manejar los argumentos variables*/
+	int		i;		
+	int		size;		
+	va_list	argument;		
 
-	if (!str)			/*se verifca si la cadena formato str esta vacia. se imprime 0, indicando que no se imprimio ningun caracter*/
+	if (!str)			
 		return (0);
-	i = 0;				/*se inicializan las varibles i y size en 0*/
+	i = 0;				
 	size = 0;
-	va_start(argument, str);	/*se inicializa argument para que apunten al primer argumento de la variable despues de str*/
-	while (str[i])			/*se inicia un bucle while que recorre a cadena formato str hasta encontrar el caracter nulo \0, que indica el final de la cadena*/
+	va_start(argument, str);	
+	while (str[i])			
 	{
-		if (str[i] == '%')	/*se verifica su e caracter actual de la cadena es un % , lo que indica un especificador de formato*/
+		if (str[i] == '%')	
 		{
-			size += ft_select_format(argument, str[i + 1]);		/*si se encuentra un %  , se llama a la funcion ft_select_format para procesar el siguiente caracter despues del % y se suma el resultado al tamaño total*/
-			i++;							/* se incrementaa i para pasar al siguiente caracter de la cadena*/
+			size += ft_select_format(argument, str[i + 1]);		
+			i++;							
 		}
-		else								/*si el caracter no es % , se llama a ft_print_char para imprimirlo y se suma al tamaño total*/
+		else							
 
 		size += ft_printf_char(str[i]);
-		i++;								/*se incrementa i para pasar al siguiente caracter de la cadena*/
+		i++;								
 	}
-	va_end(argument);		/*se finaliza el uso de la lsita de argumentos variables*/			
-	return (size);			/*la funcion retorna el tamaño total de los caracteres impresos*/
+	va_end(argument);				
+	return (size);			
 }
